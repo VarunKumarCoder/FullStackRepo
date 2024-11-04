07849ee753fabd27eda5acf8d7d4bcc6c6724bb8
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-04 23:09:46 +0530

    Employee object added

--- Proj06-Configuringproperties-array-list-set-map/src/main/java/com/cdc/Proj06ConfiguringpropertiesArrayListSetMapApplication.java
+++ Proj06-Configuringproperties-array-list-set-map/src/main/java/com/cdc/Proj06ConfiguringpropertiesArrayListSetMapApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.cdc.sbeans.Employee;
+
 @SpringBootApplication
 public class Proj06ConfiguringpropertiesArrayListSetMapApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Proj06ConfiguringpropertiesArrayListSetMapApplication.class,
 				args);
+		Employee emp = context.getBean("emp", Employee.class);
+		System.out.println(emp);
 	}
 
 }
